ff1656df277a21592a54f561b0fefaff28eb05d5
Muhammad Hamza <150447026+hamzaentterprice@users.noreply.github.com>
2025-06-03 12:54:59 +0500

    s

--- src/main/java/com/application/test/SecretController.java
+++ src/main/java/com/application/test/SecretController.java
@@ -7,22 +7,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SecretController {
 
-    // This will inject the environment variable into this field
     @Value("${my.secret:Secret not set}")
     private String mySecret;
 
     @GetMapping("/")
     public String home() {
-        // Don't do this in production - just for demo purposes
         return "Application is running with secret: " + mySecret;
     }
-    
+
     @GetMapping("/check-secret")
     public String checkSecret() {
         if (mySecret.equals("Secret not set")) {
             return "Secret is not configured";
         }
-        // In production, you'd never return the actual secret, just whether it exists
         return "Secret is configured and has " + mySecret.length() + " characters";
+    }
+
+    // This endpoint will directly display the secret value
+    @GetMapping("/display-secret")
+    public String displaySecret() {
+        return "The complete secret value is: " + mySecret;
     }
 }
--- src/main/java/com/application/test/TestApplication.java
+++ src/main/java/com/application/test/TestApplication.java
@@ -1,7 +1,10 @@
 package com.application.test;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TestApplication {
@@ -9,5 +12,13 @@ public class TestApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
 	}
-
+	@Bean
+	public CommandLineRunner displaySecretAtStartup(@Value("${my.secret:Secret not set}") String mySecret) {
+		return args -> {
+			System.out.println("========================================");
+			System.out.println("TEST MODE - DISPLAYING SECRET VALUE:");
+			System.out.println("MY_GITHUB_SECRET = " + mySecret);
+			System.out.println("========================================");
+		};
+	}
 }
