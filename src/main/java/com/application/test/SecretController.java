5e8fdf4f4bbfb93f5a6b8419a1b96ca1f5fa1fab
Muhammad Hamza <150447026+hamzaentterprice@users.noreply.github.com>
2025-06-03 12:44:22 +0500

    s

--- src/main/java/com/application/test/SecretController.java
+++ src/main/java/com/application/test/SecretController.java
@@ -1,6 +1,8 @@
 package com.application.test;
 
-
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SecretController {
